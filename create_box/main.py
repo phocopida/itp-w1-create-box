396129320006cc781aeb6cf7859fdbf12d20dfdc
Petter_Dischington <hele.verden@gmail.com>
2017-01-14 15:41:26 +0000

    finished box project

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,29 @@
 
 
 def create_box(height, width, character):
-    pass
+    box=""
+    if height > 0  and width > 0 and isinstance(character, str):
+        for number in range(height):
+           box += (width * character)+"\n"
+            
+        return box
+    else:
+        print "try again"
 
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+    
+def create_empty_box(height, width, character):
+    box=""
+    if height > 0  and width > 0 and isinstance(character, str):
+        for number in range(height):
+            if number == 0:
+                box += (width * character)+"\n"
+            elif number == height-1:
+                box += (width * character)+"\n"
+            else:
+                box += character + " "*(width-2) + character+"\n"
+        return box
+    else:
+        print "try again"
